c48f426cbc91e03837fc1483cec415bb01da1c78
Tomas Doherty <tomasdoh1@gmail.com>
2018-10-22 23:05:59 +0100

    Added index to students

--- directory.rb
+++ directory.rb
@@ -11,19 +11,19 @@ def input_students
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
-
+  
   students
 
 end
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of Villians Academy"
   puts "-------------"
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index +1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 

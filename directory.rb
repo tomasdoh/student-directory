37a2e5e0e467646d59cda9a1e25b8ac5ed6c97f1
Tomas Doherty <tomasdoh1@gmail.com>
2018-10-28 22:31:58 +0000

    adding interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,27 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+
+
 def input_students
   puts "Please enter the name of the student: "
 
@@ -27,8 +51,8 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(150)
+  puts "-------------".center(150)
 end
 
 def print(students)
@@ -38,9 +62,13 @@ def print(students)
 end
 
 def print_footer(names)
+  if names.count == 1
+    puts
   puts "Overall, we have #{names.count} great students"
+  end
 end
 
+interactive_menu
 students = input_students
 print_header
 print(students)
--- typos.rb
+++ typos.rb
@@ -7,11 +7,11 @@ def input_students
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-  # add the student hash to the array
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
-  # get another name from the user
-  name = gets.chomp
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
   end
   # return the array of students
   students
